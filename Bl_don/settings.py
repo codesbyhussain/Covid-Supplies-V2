901b3496513d806c3e57abac369fa1262f524842
Hussain Manasi <58601158+codesbyhussain@users.noreply.github.com>
2021-06-14 22:25:32 +0530

    Removed key

--- Bl_don/settings.py
+++ Bl_don/settings.py
@@ -24,7 +24,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$#6&687_5s46(i)0$_4f*&p=b$ggrc_pv^%iarqhbd(!u9*%2('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
